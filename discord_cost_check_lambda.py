ecba629b27a6f4bc2c3efca2adc191ecafa1c354
a0i_it <116965590+a0i-it@users.noreply.github.com>
2023-02-26 16:19:26 +0900

    Update USD→JPY為替変換機能

--- discord_cost_check_lambda.py
+++ discord_cost_check_lambda.py
@@ -2,6 +2,7 @@ import logging
 import json
 import urllib
 import boto3
+import math
 from datetime import datetime, date, timedelta
  
 logger = logging.getLogger()
@@ -37,6 +38,19 @@ def web_hook(messages):
         response_body = response.read().decode("utf-8")
         print(response_body)
 
+def trans_usd(cost):
+
+    return math.ceil(float(cost))
+
+def trans_yen(cost):
+    url = "https://api.aoikujira.com/kawase/json/usd"
+    request=urllib.request.Request(url)
+   
+    with urllib.request.urlopen(request) as response:
+       response_body = json.loads(response.read().decode("utf-8"))
+       print(response_body)
+    
+    return math.ceil(float(cost)*float(response_body['JPY']))
  
 ### イベントからDiscordメッセージを整形
 def parse_event(event):
@@ -52,6 +66,9 @@ def parse_event(event):
         Granularity='MONTHLY',
         Metrics=['BlendedCost'],
     )
+    
+    CostUSD = trans_usd(response['ResultsByTime'][0]['Total']['BlendedCost']['Amount'])
+    CostYEN = trans_yen(response['ResultsByTime'][0]['Total']['BlendedCost']['Amount'])
 
     message = f"""
     :moneybag: **AWSのコスト通知です** :moneybag:\r
@@ -59,7 +76,8 @@ def parse_event(event):
     ▼算出開始日 : {response['ResultsByTime'][0]['TimePeriod']['Start']}
     ▼算出終了日 : {response['ResultsByTime'][0]['TimePeriod']['End']}
     
-    ▼トータル額 : {response['ResultsByTime'][0]['Total']['BlendedCost']['Amount']} USD
+    ▼トータル額 : 約{CostUSD} USD
+    ▼トータル額 : 約{CostYEN} 円
 
     """
     logger.info('Success Creating Discord Messages!')
